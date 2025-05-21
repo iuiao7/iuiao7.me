64cde9e99e7a6dfda08f5f551075c8d1944c02da
Shawn <496224211@qq.com>
2025-05-21 11:20:40 +0800

    upgrade shadcn-nuxt

--- components/ui/tooltip/Tooltip.vue
+++ components/ui/tooltip/Tooltip.vue
@@ -1,11 +1,15 @@
 <template>
-  <TooltipRoot v-bind="forwarded">
+  <TooltipRoot
+    data-slot="tooltip"
+    v-bind="forwarded"
+  >
     <slot />
   </TooltipRoot>
 </template>
 
 <script setup lang="ts">
-import { TooltipRoot, type TooltipRootEmits, type TooltipRootProps, useForwardPropsEmits } from 'radix-vue'
+import type { TooltipRootEmits, TooltipRootProps } from 'reka-ui'
+import { TooltipRoot, useForwardPropsEmits } from 'reka-ui'
 
 const props = defineProps<TooltipRootProps>()
 const emits = defineEmits<TooltipRootEmits>()
--- components/ui/tooltip/TooltipProvider.vue
+++ components/ui/tooltip/TooltipProvider.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script setup lang="ts">
-import { TooltipProvider, type TooltipProviderProps } from 'radix-vue'
+import type { TooltipProviderProps } from 'reka-ui'
+import { TooltipProvider } from 'reka-ui'
 
-const props = defineProps<TooltipProviderProps>()
+const props = withDefaults(defineProps<TooltipProviderProps>(), {
+  delayDuration: 0,
+})
 </script>
