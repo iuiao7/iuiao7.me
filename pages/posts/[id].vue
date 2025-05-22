29b766868dd46b3964fb0d1a54b194f5dcb5b94f
Shawn <496224211@qq.com>
2025-05-22 17:43:32 +0800

    feat: add dynamic posts pages and API endpoints for fetching post data, including new dependencies for markdown processing

---
+++ pages/posts/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>{{ data.title }}</h1>
+    <div v-html="data.content" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const data = await $fetch(`/api/posts/${route.params.id}`)
+</script>
---
+++ server/api/posts/[id].ts
@@ -0,0 +1,28 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import matter from 'gray-matter'
+import rehypeStringify from 'rehype-stringify'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(rehypeStringify)
+
+const postsDir = path.join(process.cwd(), 'content/1.writings')
+
+export default defineEventHandler(async (event) => {
+  const fileName = `${getRouterParam(event, 'id')}.md`
+  const fullPath = path.join(postsDir, fileName)
+  const fileContent = fs.readFileSync(fullPath, { encoding: 'utf-8' })
+  const { data: frontmatter, content } = matter(fileContent)
+  const file = await processor.process(content)
+
+  return {
+    title: frontmatter.title,
+    content: file.value,
+  }
+})
